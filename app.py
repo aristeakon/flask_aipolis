67d10c24b77ee5570098d00d395a10c4d239d2b0
aristeakon <aristeabd@hotmail.com>
2020-06-15 20:10:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template
+from tensorflow.keras.models import load_model
+import tensorflow as tf
+from flask import request
+from flask import jsonify
+from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
   return render_template('hi.html')
+
+def get_model():
+    global model
+    model = load_model('saved_model_aipolis.h5')
+    print(" * Model loaded!")
+
+print(" * Loading Keras model...")
+get_model()
 
 if __name__ == "__main__":
     app.run(debug=False)
