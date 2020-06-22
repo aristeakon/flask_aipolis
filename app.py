9c25906db2fbb1abe8811e91b56dd6f2e8408aeb
aristeakon <aristeabd@hotmail.com>
2020-06-22 19:07:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,23 @@ def index():
   print(" * hi")
   return render_template('index.html')
 
+@app.route('/admin',methods=['POST','GET'])
+def checkDate():
+  inputid=int(request.args.get('userid'))
+  inputpoi=int(request.args.get('poiid'))
+  model = load_model('saved_model_aipolis.h5')
+  print(" * Loading Keras model...")
+  print(" * Model loaded!")
+  input1 = tf.constant([[inputid]],dtype=tf.int32)
+  input2 = tf.constant([[inputpoi]],dtype=tf.int32)
+  predictions = model.predict([input1, input2],steps=1).tolist()
+  response = {
+        'predictions': {
+            'rating': predictions
+        }
+    }
+  return jsonify(response)
+  
 @app.route("/predict1", methods=["POST"])
 def predict():
     message = request.get_json(force=True)
