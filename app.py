4fce5a322286b07497e3d0dd1aa5b85d4c4f0abc
aristeakon <aristeabd@hotmail.com>
2020-06-22 18:43:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,24 +17,6 @@ def index():
 def checkDate():
     return 'From Date is'+request.args.get('from_date')+ ' To Date is '+ request.args.get('to_date')
 
-@app.route('/admin',methods=['POST','GET'])
-def checkDate():
-    inputid=int(request.args.get('userid'))
-    inputpoi=int(request.args.get('poiid'))
-    #return 'From Date is'+request.args.get('from_date')+ ' To Date is '+ request.args.get('to_date')
-    model = load_model('saved_model_aipolis.h5')
-    print(" * Loading Keras model...")
-    print(" * Model loaded!")
-    input1 = tf.constant([[inputid]],dtype=tf.int32)
-    input2 = tf.constant([[inputpoi]],dtype=tf.int32)
-    predictions = model.predict([input1, input2]).tolist()
-    response = {
-        'predictions': {
-            'rating': predictions
-        }
-    }
-    return jsonify(response)
-
 @app.route("/predict1", methods=["POST"])
 def predict():
     message = request.get_json(force=True)
