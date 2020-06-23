aeb1c1af954540e17cebe568fc74a00a02810ccc
aristeakon <aristeabd@hotmail.com>
2020-06-23 14:19:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,28 @@ def checkDate():
         }
     }
   return jsonify(response)
+
+@app.route('/topfivepr',methods=['POST','GET'])
+def checkTopfivepr():
+  get_pois()
+  model = load_model('saved_model_aipolis.h5')
+  print(" * Loading Keras model...")
+  print(" * Model loaded!")
+  inputid=int(request.args.get('userid'))
+  poi_data = np.array(list(set(dataset.poi_id)))
+  user = np.array([inputid for i in range(len(poi_data))])
+  predictions = model.predict([user, poi_data]) 
+  predictions = np.array([a[0] for a in predictions])
+  recommended_poi_ids = (-predictions).argsort()[:5]
+  data=[]
+  for feature in recommended_poi_ids:
+    response = {
+      'poiid':  int(poi_data[feature]),
+      'rating': float(predictions[feature])
+    }
+    data.append(response) 
+    jsonData=json.dumps(data)
+    return jsonData
   
 @app.route("/predict1", methods=["POST"])
 def predict():
