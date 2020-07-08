dbdaea94d54db2e652d52800b0bb26d77e97622f
aristeakon <aristeabd@hotmail.com>
2020-07-08 11:32:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -41,6 +41,30 @@ def checkTopfivepr():
   jsonData=json.dumps(data)
   return jsonData
 
+@app.route('/topfivepr1',methods=['POST','GET'])
+def checkTopfivepr1():
+  get_pois()
+  mobilenet_save_path="saved_model_mf_adam"
+  model = load_model(mobilenet_save_path)
+  print(" * Model loaded!")
+  inputid=int(request.args.get('userid'))
+  poi_data = np.array(list(set(dataset.poi_id)))
+  user = np.array([inputid for i in range(len(poi_data))])
+  predictions = model.predict([user, poi_data])
+  predictions = np.array([a[0] for a in predictions])
+  recommended_poi_ids = (-predictions).argsort()[:5]
+  print(predictions[recommended_poi_ids])
+  print(poi_data[recommended_poi_ids])
+  data=[]
+  for feature in recommended_poi_ids:
+    response = {
+      'poiid':  int(poi_data[feature]),
+      'rating': float(predictions[feature])
+    }
+    data.append(response) 
+  jsonData=json.dumps(data)
+  return jsonData
+
 @app.route('/all',methods=['POST','GET'])
 def returnall():
   get_pois()
