2ef2539bf65cd2d1541f95e1fd0ae5805a3a53e4
aristeakon <aristeabd@hotmail.com>
2020-07-08 10:24:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,30 +18,6 @@ def get_pois():
   global dataset
   dataset = pd.read_csv('data/ratings.csv')
   
-@app.route("/predict1", methods=["POST"])
-def predict():
-    message = request.get_json(force=True)
-    inputid=int(message['userid'])
-    inputpoi=int(message['poiid'])
-    global model
-    model = load_model('saved_model_aipolis.h5')
-    print(" * Loading Keras model...")
-    print(" * Model loaded!")
-    input1 = tf.constant([[inputid]],dtype=tf.int32)
-    input2 = tf.constant([[inputpoi]],dtype=tf.int32)
-    predictions = model.predict([input1, input2],steps=1).tolist()
-   # for x in range(1, 10):
-    #   input2 = tf.constant([[x]],dtype=tf.int32)
-    #   predictions = model.predict([input1, input2]).tolist()
-    #   print(predictions)
-
-    response = {
-        'predictions': {
-            'rating': predictions
-        }
-    }
-    return jsonify(response)
-  
 @app.route('/all',methods=['POST','GET'])
 def returnall():
   get_pois()
